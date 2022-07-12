1155f69cd6059001db17af2d31f305450cc3519d
Jose-cmd98 <jose1234roberto@gmail.com>
2022-07-12 15:57:18 -0300

    inside plataform

--- src/app/views/header/header.component.ts
+++ src/app/views/header/header.component.ts
@@ -1,3 +1,6 @@
+import { TokenService } from './../../core/service/token-service/token.service';
+import { Router } from '@angular/router';
+import { UserService } from './../../core/service/user-service/user.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  userName!: string;
+
+  constructor(
+    private userService: UserService,
+    private tokenService: TokenService,
+    private route: Router
+    ) { }
 
   ngOnInit(): void {
+    this.userName = this.tokenService.getUser();
+    console.log(this.userName);
+  }
+
+  logout(){
+    this.userService.logout();
+    this.route.navigate(['/login']);
   }
 
 }
